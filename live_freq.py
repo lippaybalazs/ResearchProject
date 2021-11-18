b650f8659aad69b3b06921ff4a35b2db9d273ce4
Lippay Balazs <lippaybalazs@gmail.com>
2021-11-18 16:06:30 +0200

    added comments to code

--- live_freq.py
+++ live_freq.py
@@ -20,14 +20,20 @@ stream = pa.open(
 )
 
 
+
+# set up frequency array
+x = np.fft.fftfreq(CHUNK, d = 1.0 / RATE)
+
+
+# set up first set of data
 data = stream.read(CHUNK)
 data = np.frombuffer(data, np.float32)
-
-x = np.fft.fftfreq(CHUNK, d = 1.0 / RATE)
 y = np.abs(np.fft.fft(data))/ (RATE / CHUNK)
+
+# noise gate
 y[y < 2.5] = 0
 
-
+# create plot window
 plt.ion()
 fig, ax = plt.subplots()
 line, = ax.plot(x, y)
@@ -35,17 +41,23 @@ ax.set_xlim(0,2000)
 ax.set_ylim(0,20)
 
 while True:
+    # read chunk of data
     data = stream.read(CHUNK)
     data = np.frombuffer(data, np.float32)
 
+    # process data
     y = np.abs(np.fft.fft(data))/ (RATE / CHUNK)
+    # noise gate
     y[y < 2.5] = 0
 
+    # plot data
     line.set_ydata(y)
     fig.canvas.draw()
     fig.canvas.flush_events()
 
+    # get peaks of array
     max_ind = argrelextrema(y, np.greater)
     r = y[max_ind]
     if (len(r) > 0):
+        # print frequency
         print(np.abs(x[np.where(y == r[0])[0][0]]))
